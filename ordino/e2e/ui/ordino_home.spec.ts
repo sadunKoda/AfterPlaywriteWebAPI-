368bfb2d604a2686880abdc85b083b2ab1178e33
sandun <msadunkodagoda@gmail.com>
2025-12-23 13:06:36 +0530

    test 3

--- ordino/e2e/ui/ordino_home.spec.ts
+++ ordino/e2e/ui/ordino_home.spec.ts
@@ -9,4 +9,33 @@ test.describe('Ordino Home Dashboard - Test Suite', () => {
         await loginPage.step_clickLogin();           
         await homePage.step_logout();
     });    
+
+     test('Verify Profile Logout v2', async ({ loginPage, homePage }) => {
+        await loginPage.goto();
+        await loginPage.step_enterUsername("admin@ddorm.com");
+        await loginPage.step_enterPassword("admin");
+        await loginPage.step_clickLogin();           
+        await homePage.step_logout();
+    });    
+     test('Verify Profile Logout 3' , async ({ loginPage, homePage }) => {
+        await loginPage.goto();
+        await loginPage.step_enterUsername("admin@dd.com");
+        await loginPage.step_enterPassword("admin");
+        await loginPage.step_clickLogin();           
+        await homePage.step_logout();
+    });    
+     test('Verify Profile Logout 4', async ({ loginPage, homePage }) => {
+        await loginPage.goto();
+        await loginPage.step_enterUsername("admin@pxx.com");
+        await loginPage.step_enterPassword("admin");
+        await loginPage.step_clickLogin();           
+        await homePage.step_logout();
+    });    
+     test('Verify Profile Logout 5', async ({ loginPage, homePage }) => {
+        await loginPage.goto();
+        await loginPage.step_enterUsername("admin@pxxxrm.com");
+        await loginPage.step_enterPassword("admin");
+        await loginPage.step_clickLogin();           
+        await homePage.step_logout();
+    });    
 });
