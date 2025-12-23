c19859b18ce2f6f66aa2959457589002c9879235
sandun <msadunkodagoda@gmail.com>
2025-12-23 13:01:46 +0530

    test commit

--- ordino/e2e/ui/ordino_login.spec.ts
+++ ordino/e2e/ui/ordino_login.spec.ts
@@ -17,4 +17,47 @@ test.describe('Ordino Auth Sign-in - Test Suite', () => {
     await loginPage.step_clickLogin()
   })
 
+    test('Auth Sign-in with Valid Credentials 1 ', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin@platform.com")
+    await loginPage.step_enterPassword("admin")
+    await loginPage.step_clickLogin() 
+  });
+
+  test('Auth Sign-in with In-Valid Credentials 3 ', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin")
+    await loginPage.step_enterPassword("wrongPassword")
+    await loginPage.validate_login_error()
+    await loginPage.step_clickLogin()
+  })
+
+   test('Auth Sign-in with Valid Credentials error1', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin@xxxxx.com")
+    await loginPage.step_enterPassword("admin")
+    await loginPage.step_clickLogin() 
+  });
+
+   test('Auth Sign-in with Valid Credentials error2', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin@xxxxx.com")
+    await loginPage.step_enterPassword("admin")
+    await loginPage.step_clickLogin() 
+  });
+
+  test('Auth Sign-in with Valid Credentials error3', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin@xxxxx.com")
+    await loginPage.step_enterPassword("admin")
+    await loginPage.step_clickLogin() 
+  });
+
+  test('Auth Sign-in with Valid Credentials error4', async ({ loginPage, homePage }) => {
+    await loginPage.goto()
+    await loginPage.step_enterUsername("admin@xxxxx.com")
+    await loginPage.step_enterPassword("admin")
+    await loginPage.step_clickLogin() 
+  });
+
 });
